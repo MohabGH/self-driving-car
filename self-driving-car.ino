8a59f35017a8499a926b16d8d218edb1990781d4
Mohab <learn.mohab.things@gmail.com>
2025-05-15 12:06:31 +0300

    added speed to the motor

--- self-driving-car.ino
+++ self-driving-car.ino
@@ -1,22 +1,28 @@
-#define DIGITAL_PINS 4
+#define DIGITAL_PINS 5
 #define POSITIVE_PIN_M1 11
 #define NEGATIVE_PIN_M1 12
+#define SPEED_PIN_M1 5
+#define SPEED_PIN_M2 6
 #define POSITIVE_PIN_M2 10
 #define NEGATIVE_PIN_M2 9
+#define NO_PIN -1
 #define FORWARD 1
 #define BACKWARD 0
 #define RIGHT 1
 #define LEFT 0
 
+/*A motor data type the that its pins get information from an H bridge.*/
 typedef struct{
   uint8_t positivePin;
   uint8_t negativePin;
+  uint8_t speedPin;
 } Motor_t;
 
-void motorInit(Motor_t *motor, uint8_t positivePin, uint8_t negativePin);
-void rotateMotor(Motor_t *motor, uint8_t rotateMode);
+void motorInit(Motor_t *motor, uint8_t positivePin, uint8_t negativePin, uint8_t speedPin);
+void rotateMotor(Motor_t *motor, uint8_t rotateMode, uint8_t speed);
 void stopMotor(Motor_t *motor);
-void steerMotor(Motor_t *rightMotor, Motor_t *leftMotor, uint8_t steerMode);
+void steerMotor(Motor_t *rightMotor, Motor_t *leftMotor, uint8_t steerMode, uint8_t steerSpeed);
+void changeMotorSpeed(Motor_t *motor, uint8_t speed);
 
 Motor_t rightMotor;
 Motor_t leftMotor;
@@ -30,7 +36,8 @@ void setup() {
     POSITIVE_PIN_M1,
     NEGATIVE_PIN_M1,
     POSITIVE_PIN_M2,
-    NEGATIVE_PIN_M2
+    NEGATIVE_PIN_M2,
+    SPEED_PIN_M1
   };
   for (uint8_t i = 0; i < DIGITAL_PINS; i++)
   {
@@ -38,35 +45,36 @@ void setup() {
   }
 
   // Setting up motors.
-  motorInit(&rightMotor, POSITIVE_PIN_M1, NEGATIVE_PIN_M1);
-  motorInit(&leftMotor, POSITIVE_PIN_M2, NEGATIVE_PIN_M2);
+  motorInit(&rightMotor, POSITIVE_PIN_M1, NEGATIVE_PIN_M1, SPEED_PIN_M1);
+  motorInit(&leftMotor, POSITIVE_PIN_M2, NEGATIVE_PIN_M2, SPEED_PIN_M2);
 }
 
 // TODO: 
 void loop() {
   // put your main code here, to run repeatedly:
-  rotateMotor(&rightMotor, FORWARD);
+  rotateMotor(&rightMotor, FORWARD, 255);
   delay(1000);
   stopMotor(&rightMotor);
   delay(1000);
-  rotateMotor(&leftMotor, BACKWARD);
+  rotateMotor(&leftMotor, BACKWARD, 128);
   delay(1000);
   stopMotor(&leftMotor);
   delay(1000);
 }
 
 /*Initialize the motor terminals according to the pins used from H bridge motor driver.*/
-void motorInit(Motor_t *motor, uint8_t positivePin, uint8_t negativePin)
+void motorInit(Motor_t *motor, uint8_t positivePin, uint8_t negativePin, uint8_t speedPin)
 {
   motor->positivePin = positivePin;
   motor->negativePin = negativePin;
+  motor->speedPin = speedPin;
 }
 
 /*Rotates a motor in a specific direction:
   rotateMode == 1 -> forward,
   rotateMode == 0 -> backward,
   rotateMode != 1, 0 -> Invalid.*/
-void rotateMotor(Motor_t *motor, uint8_t rotateMode)
+void rotateMotor(Motor_t *motor, uint8_t rotateMode, uint8_t speed)
 {
   if(rotateMode != FORWARD && rotateMode != BACKWARD)
   {
@@ -76,6 +84,7 @@ void rotateMotor(Motor_t *motor, uint8_t rotateMode)
 
   digitalWrite(motor->positivePin, rotateMode % 2);
   digitalWrite(motor->negativePin, (rotateMode + 1) % 2);
+  if(motor->speedPin != -1) analogWrite(motor->speedPin, speed);
 }
 
 // Stops the motor by making both the pins it is connected to into low.
@@ -89,7 +98,7 @@ void stopMotor(Motor_t *motor)
   steerMode = 1 -> steer right,
   steerMode = 0 -> steer left,
   steerMode != 1, 0 -> Invalid.*/
-void steerMotor(Motor_t *rightMotor, Motor_t *leftMotor, uint8_t steerMode)
+void steerMotor(Motor_t *rightMotor, Motor_t *leftMotor, uint8_t steerMode, uint8_t steerSpeed)
 {
   if(steerMode != RIGHT && steerMode != LEFT)
   {
@@ -97,6 +106,12 @@ void steerMotor(Motor_t *rightMotor, Motor_t *leftMotor, uint8_t steerMode)
     return;
   }
 
-  rotateMotor(rightMotor, (steerMode + 1) % 2);
-  rotateMotor(leftMotor, steerMode % 2);
+  rotateMotor(rightMotor, (steerMode + 1) % 2, steerSpeed);
+  rotateMotor(leftMotor, steerMode % 2, steerSpeed);
+}
+
+/*Changes the speed of a specific motor without using rotateMotor function again.*/
+void changeMotorSpeed(Motor_t *motor, uint8_t speed)
+{
+  analogWrite(motor->speedPin, speed);
 }
